cd52f0b84c89160e774e958853efd6054b7472e4
kshershen <shkipc@gmail.com>
2013-11-24 20:42:51 +0400

    correct STATICFILES_DIRS

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -80,12 +80,13 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    'g:/work/py/django-blog/static',
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+if DEBUG:
+    STATICFILES_DIRS = (
+        'g:/work/py/django-blog/static',
+        # Put strings here, like "/home/html/static" or "C:/www/django/static".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+    )
 
 # List of finder classes that know how to find static files in
 # various locations.
