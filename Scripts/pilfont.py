cb327e05912a417766567b16f158741c7218f5cd
kshershen <shkipc@gmail.com>
2013-12-17 22:22:06 +0400

    migrate to pythonanywhere.com, deleted virtualenv

--- Scripts/pilfont.py
+++
@@ -1,5 +0,0 @@
-#!G:\work\py\django-blog\Scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'Pillow==2.2.1','pilfont.py'
-__requires__ = 'Pillow==2.2.1'
-import pkg_resources
-pkg_resources.run_script('Pillow==2.2.1', 'pilfont.py')
