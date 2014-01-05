0346557f57ddfeee858b26c7400881f865a57c75
kshershen <shkipc@gmail.com>
2014-01-05 20:13:16 +0400

    fix archives

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('blog.views',
     url(r'^blog/page(?P<page>\d+)/$', PostListView.as_view(queryset=Post.publics.all(), template_name='blog.html')),
     url(r'^(?P<pk>\d+)$', DetailView.as_view(model=Post, template_name='post_detail.html')),
     url(r'^archives/$', ListView.as_view(queryset=Post.publics.all(), template_name='archives.html')),
+    url(r"^archives/(\d+)/(\d+)/$", "month"),
     url(r'^tag/(?P<tag>\w+)$', 'tagpage'),
 )
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from blog.models import Post
 from django.views.generic import ListView
-
+import datetime
+from calendar import monthrange
 
 def index(request):
 	return render(request, 'index.html')
@@ -18,5 +19,5 @@ class PostListView(ListView):
 
 def month(request, year, month):
     """Monthly archive."""
-    posts = Post.publics.filter(created__year=year, created__month=month)
-    return render_to_response("archives.html", dict(post_list=posts, user=request.user, archive=True))
+    object_list = Post.publics.filter(created__range = (datetime.date(int(year),int(month),1),datetime.date(int(year),int(month),monthrange(int(year), int(month))[1])))
+    return render(request, "archives.html", locals())
