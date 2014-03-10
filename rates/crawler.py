a569b87258af84da31447c38ec4487c1d6d87092
kshershen <shkipc@gmail.com>
2014-03-10 21:23:46 +0400

    change crawler to monothread

--- rates/crawler.py
+++ rates/crawler.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 import os, urllib2, yql, sys, time
-import thread, threading
 from decimal import Decimal
 from djangoproject import settings
 from rates.models import Bank, BankRate
+import httplib2
 
 class RateParser():
 
     def __init__(self, check_date):
         self.banks = []
-        self.threads = 0
         self.date = check_date
 
     def do_parse(self):
@@ -17,32 +16,20 @@ class RateParser():
         for b in banks:
             self.banks.append(b)
         while (len(self.banks)>0):
-            if (settings.THREADS_COUNT > self.threads):
-                bank = self.banks.pop()
-                self.threads += 1
-                thread.start_new_thread(self.get_rate, (bank,))
-            else:
-                pass
-
-    def get_rate(self, bank):
-        self.get_from_bank(bank)
-        self.threads -= 1
+            bank = self.banks.pop()
+            self.get_from_bank(bank)
 
     def get_from_bank(self, bank):
-
+        
         bank_info = bank
         if bank_info.URL == 'none' or bank_info.URL == '':
             exit
-        print bank_info.xpath
         if bank_info.xpath != 'none' and bank_info.xpath != '':
             try:
-                print bank_info.name                
-                site = y = yql.Public()
+                site = y = yql.Public(httplib2.Http())
                 query = 'select * from html where url="%s" and xpath="%s"' % (bank_info.URL, bank_info.xpath)
                 result = y.execute(query)
-                print result.results
-                if result.results:
-                    print result.rows
+                if result and result.results:
                     if  bank_info.name in ['Восточный экспресс'] :
                         val = result.results.values()[0][0]
                     elif  bank_info.name in ['КУРСКПРОМБАНК', 'Нордеа Банк', 'Банк Россия', 'ВТБ 24', 'Инвестторгбанк', 'Липецккомбанк', 'ЛОКО-БАНК', 'МАСТ-Банк',
@@ -69,9 +56,8 @@ class RateParser():
                             val = Decimal(val)
                         except:
                             val = 0
-                        print str(val)
                         p = BankRate(bank=bank_info, rate="USD", value=val, checktime=self.date)
                         p.save()
                         
             except Exception, err:
-                print 'error: %s'%err
+                print '%s: error: %s'%(bank_info.name, err)
