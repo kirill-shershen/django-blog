6b92163ea060cb0a7ebe3f7340f1dff34bcb3a0d
kxekxe <shkipc@gmail.com>
2014-10-02 20:14:58 +0400

    added cbr.ru currency for rates

--- rates/views.py
+++ rates/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from rates.models import Bank, BankContact, BankRate
+from cbrcurrency import GetCurrentCurrency
 # Create your views here.
 def LastRates(request):
     last_time = BankRate.objects.order_by("-checktime")[0]
     banks = Bank.objects.all()
     bankcontacts = BankContact.objects.all()
+    current_currency = GetCurrentCurrency()
     rates = BankRate.objects.filter(checktime=last_time.checktime).order_by('-value')
     return render(request, "rates.html", locals())
