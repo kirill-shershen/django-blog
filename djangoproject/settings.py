b1716ec0d4adfe39bb87a3099a8b59bce921c84b
kshershen <shkipc@gmail.com>
2017-01-15 23:07:51 +0300

    fix collectstatic

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -255,7 +255,7 @@ if os.environ['HOST'] == 'heroku':
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = 'http://%s/' % AWS_STORAGE_BUCKET_NAME
